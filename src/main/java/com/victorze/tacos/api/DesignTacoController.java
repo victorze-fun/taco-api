adaae27defbc671cbf4f3519b66322b55b045531
Victor <victorzevallos@protonmail.com>
2020-11-01 10:58:35 -0500

    Add post handler in DesignTacoController

--- src/main/java/com/victorze/tacos/api/DesignTacoController.java
+++ src/main/java/com/victorze/tacos/api/DesignTacoController.java
@@ -10,7 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victorze.tacos.Taco;
@@ -37,6 +40,13 @@ public class DesignTacoController {
 			return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
 		}
 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	}
+	
+	@PostMapping(consumes="application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Taco postTaco(@RequestBody Taco taco) {
+		System.out.println("foooooo");
+		return tacoRepository.save(taco);
 	}
 
 }
--- src/main/java/com/victorze/tacos/security/SecurityConfig.java
+++ src/main/java/com/victorze/tacos/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.victorze.tacos.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,22 +28,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/design", "/orders")
-					.access("hasRole('ROLE_USER')")
-				.antMatchers("/", "/**", "/h2-console/**").permitAll()
+				.antMatchers(HttpMethod.OPTIONS).permitAll() // needed for Angular/CORS
+				.antMatchers("/design", "/orders").permitAll()
+				.antMatchers(HttpMethod.PATCH, "/ingredients").permitAll()
+		        .antMatchers("/**").access("permitAll")
 				
 			.and()
 				.formLogin()
 					.loginPage("/login")
-					
+			.and()
+				.httpBasic()
+					.realmName("Taco Cloud")
 			.and()
 				.logout()
 					.logoutSuccessUrl("/")
+			.and()
+				.csrf()
+					.ignoringAntMatchers("/h2-console/**", "/ingredients/**", "/design", "/orders/**")
 			;
 		
+			http.headers().frameOptions().sameOrigin();
 			// Active h2-console
-			http.csrf().disable();
-			http.headers().frameOptions().disable();
+//			http.csrf().disable();
+//			http.headers().frameOptions().disable();
 	}
 
 	@Override
